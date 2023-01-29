c3f5cad89079dd0f31670db3078e9fafa6865f6a
Laurent Sellin <lsellin@cisco.com>
2023-01-29 15:03:15 +0100

    solved exercise

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,14 +1,32 @@
 package com.example.android.background.sync;
 
-// TODO (1) Create a class called ReminderTasks
+//  (1) Create a class called ReminderTasks
 
-// TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+import android.content.Context;
 
-// TODO (6) Create a public static void method called executeTask
-// TODO (7) Add a Context called context and String parameter called action to the parameter list
+import com.example.android.background.utilities.PreferenceUtilities;
 
-// TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+public class ReminderTasks {
+    public static final String ACTION_INCREMENT_WATER_COUNT="increment-water-count";
+    private static void incrementWaterCount(Context context){
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+    public static void executeTask(Context context, String action){
+        if (action.equals(ACTION_INCREMENT_WATER_COUNT)) {
+            incrementWaterCount(context);
+        }
 
-// TODO (3) Create a private static void method called incrementWaterCount
-// TODO (4) Add a Context called context to the argument list
-// TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+    }
+
+}
+
+//  (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+
+//  (6) Create a public static void method called executeTask
+//  (7) Add a Context called context and String parameter called action to the parameter list
+
+//  (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+
+//  (3) Create a private static void method called incrementWaterCount
+//  (4) Add a Context called context to the argument list
+//  (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
